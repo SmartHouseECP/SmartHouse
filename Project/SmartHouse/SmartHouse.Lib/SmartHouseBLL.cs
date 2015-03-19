d18fbd93537520a3d338a2fa64ae17a8fc43682b
thaianandreia <thaianandreia@hotmail.com>
2015-03-19 09:39:16 -0300

    Adição do Projeto SmartHouse no repositório

---
+++ Project/SmartHouse/SmartHouse.Lib/SmartHouseBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+
+namespace SmartHouse
+{
+
+    public class SmartHouseBLL
+    {
+        string SQLConnection = @"Data Source=smarthouse.ctxqmmzfu2jc.us-west-2.rds.amazonaws.com,1433;
+                                Initial Catalog=SmartHouse;User ID=SmartHouse;Password=SmartHouse";
+        protected DbContext context { get; set; }
+        public SmartHouseBLL()
+        {
+            context = new DbContext(SQLConnection);
+        }
+    }
+}
